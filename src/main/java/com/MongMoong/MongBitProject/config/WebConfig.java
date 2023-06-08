49ebfbd088d19bd12a4fc8efcf2366ad62d8581c
WillNeiman <willneimann@gmail.com>
2023-06-08 18:45:30 +0900

    fix: CorsMapping

--- src/main/java/com/MongMoong/MongBitProject/config/WebConfig.java
+++ src/main/java/com/MongMoong/MongBitProject/config/WebConfig.java
@@ -1,6 +1,7 @@
 package com.MongMoong.MongBitProject.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -13,6 +14,7 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedOrigins("https://mongbit-frontend-moorisong.koyeb.app", "http://localhost:3000") // 리액트 애플리케이션의 URL을 여기에 입력하세요.
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                 .allowedHeaders("*")
+                .exposedHeaders(HttpHeaders.AUTHORIZATION) // 권한 헤더 노출 설정
                 .allowCredentials(true);
     }
 }
