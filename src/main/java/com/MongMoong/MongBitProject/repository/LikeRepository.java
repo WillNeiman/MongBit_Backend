13abfb68043d1209e9f7668e329fceb34a8f4625
WillNeiman <willneimann@gmail.com>
2023-06-13 11:46:23 +0900

    add: like service, repository, controller

--- src/main/java/com/MongMoong/MongBitProject/repository/LikeRepository.java
+++ src/main/java/com/MongMoong/MongBitProject/repository/LikeRepository.java
@@ -4,4 +4,7 @@ import com.MongMoong.MongBitProject.model.Like;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface LikeRepository extends MongoRepository<Like, String> {
+
+    int countByTestId(String testId);
+    Like findByMemberIdAndTestId(String memberId, String testId);
 }
