56f2eed0b6273117bb078398f0fc17e208669b7e
Will Neiman <willneimann@gmail.com>
2023-06-16 15:50:16 +0900

    fix: bugfix (API spec)

--- src/main/java/com/MongMoong/MongBitProject/controller/CommentController.java
+++ src/main/java/com/MongMoong/MongBitProject/controller/CommentController.java
@@ -39,14 +39,14 @@ public class CommentController {
         return ResponseEntity.noContent().build();
     }
 
-    @GetMapping("/{testId}/comments")
+    @GetMapping("/comments")
     @Operation(summary = "특정 테스트에 대한 모든 댓글 조회", description = "Comment객체 안에 testId가 필요합니다")
     public ResponseEntity<List<CommentResponse>> getCommentList(@RequestBody Comment comment) {
         List<CommentResponse> commentResponses = commentService.getCommentsForTest(comment);
         return ResponseEntity.ok(commentResponses);
     }
 
-    @GetMapping("/{testId}/comments/page/{pageNumber}")
+    @GetMapping("/comments/page/{pageNumber}")
     @Operation(summary = "특정 테스트에 대한 댓글 조회 (페이지당 10개)", description = "Comment객체 안에 testId와 페이지 번호가 필요합니다.")
     public ResponseEntity<List<CommentResponse>> getCommentListPaged(@RequestBody Comment comment, @PathVariable int pageNumber) {
         List<CommentResponse> commentResponses = commentService.getCommentsForTestPaged(comment, pageNumber);
