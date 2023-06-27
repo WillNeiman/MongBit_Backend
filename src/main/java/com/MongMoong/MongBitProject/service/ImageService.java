660faa422114dfbbf19b1c013b89a2419167e0f4
WillNeiman <willneimann@gmail.com>
2023-06-27 17:22:05 +0900

    fix: image upload rate limit error solved

--- src/main/java/com/MongMoong/MongBitProject/service/ImageService.java
+++ src/main/java/com/MongMoong/MongBitProject/service/ImageService.java
@@ -1,7 +1,9 @@
 package com.MongMoong.MongBitProject.service;
 
+import com.MongMoong.MongBitProject.config.ImgbbConfig;
 import com.MongMoong.MongBitProject.dto.ImageUploadResponse;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
 import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
@@ -12,24 +14,26 @@ import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.InputStream;
+import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class ImageService {
 
-    @Value("${imgbb.key}")
-    private String API_KEY;
+    private int index = 0;
+    private final ImgbbConfig imgbbConfig;
 
     public String uploadImageToImgBB(MultipartFile file) throws Exception {
-        System.out.println("API_KEY = " + API_KEY);
+        String key = getNextKey();
         InputStream in = file.getInputStream();
 
         HttpPost post = new HttpPost("https://api.imgbb.com/1/upload");
-        System.out.println("post = " + post);
         MultipartEntityBuilder builder = MultipartEntityBuilder.create();
-        builder.addTextBody("key", API_KEY);
+        builder.addTextBody("key", key);
 
         ContentType contentType = ContentType.create(file.getContentType());
         builder.addBinaryBody("image", in, contentType, file.getOriginalFilename());
@@ -41,12 +45,23 @@ public class ImageService {
         CloseableHttpResponse response = httpClient.execute(post);
         HttpEntity responseEntity = response.getEntity();
 
-        System.out.println("responseEntity = " + responseEntity);
         String result = EntityUtils.toString(responseEntity);
 
         ObjectMapper mapper = new ObjectMapper();
         ImageUploadResponse uploadResponse = mapper.readValue(result, ImageUploadResponse.class);
 
+        if (uploadResponse.getStatus_code() == 400 && uploadResponse.getError().getCode() == 100) {
+            return uploadImageToImgBB(file); // 재귀적으로 본 메소드 다시 호출
+        }
+
         return uploadResponse.getData().getUrl();
+    }
+
+    private String getNextKey() {
+        List<String> keys = imgbbConfig.getKeys(); // ImageBB api key 가져오기
+        if (index >= keys.size()) {
+            index = 0;
+        }
+        return keys.get(index++);
     }
 }
